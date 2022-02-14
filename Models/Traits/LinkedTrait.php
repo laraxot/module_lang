258225d1ea82476d3fe4793eb43520d936ea9fee
marco76tv <marco.sottana@gmail.com>
2022-02-14 18:11:03 +0100

    .

--- Models/Traits/LinkedTrait.php
+++ Models/Traits/LinkedTrait.php
@@ -22,7 +22,7 @@ use Modules\Lang\Models\Post;
 use Modules\LU\Models\User;
 use Modules\Tenant\Services\TenantService;
 use Modules\Xot\Models\Image;
-use Modules\Xot\Services\PanelService as Panel;
+use Modules\Xot\Services\PanelService;
 use Modules\Xot\Services\RouteService;
 
 // per dizionario morph
@@ -59,7 +59,7 @@ trait LinkedTrait
 
         if (false === $alias) {
             $data = [];
-            $panel = Panel::get($this);
+            $panel = PanelService::get($this);
             $alias = $panel->postType();
             $data['model'][$alias] = $class;
             TenantService::saveConfig(['name' => 'xra', 'data' => $data]);
@@ -324,7 +324,7 @@ trait LinkedTrait
     public function getUrlAttribute($value) {
 
         //return $this->getPostAttr(__FUNCTION__, $value);
-        return Panel::get($this)->url();
+        return PanelService::get($this)->url();
     }
 
     //*/
@@ -539,7 +539,7 @@ trait LinkedTrait
     {
         $item_guid = str_replace('%20', '%', $item_guid);
         $item_guid = str_replace(' ', '%', $item_guid);
-        $panel = Panel::get($this);
+        $panel = PanelService::get($this);
         $other_lang = Post::query()
             ->where('post_type', $panel->postType())
             ->where('guid', 'like', $item_guid)
