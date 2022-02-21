46ff197cd03da547dd0d2fc427c405c0abb6b938
Malebestia <malebestia@yahoo.it>
2022-02-21 22:15:16 +0100

    .

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('lang::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('lang.name') !!}
+    </p>
+@endsection
