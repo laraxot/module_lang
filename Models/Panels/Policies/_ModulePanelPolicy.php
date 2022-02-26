68856d01d361e8682461f84ec04586d62250fe89
Malebestia <malebestia@yahoo.it>
2022-02-26 19:43:58 +0100

    .

---
+++ Models/Panels/Policies/_ModulePanelPolicy.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Lang\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Lang\Models\Panels\Policies\_ModulePanelPolicy as Panel;
+
+use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
+
+class _ModulePanelPolicy extends XotBasePanelPolicy {
+}
