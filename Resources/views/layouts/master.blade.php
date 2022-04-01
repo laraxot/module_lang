eec8c0ace735303918db1412ba3c2bde1e4e55a6
marco76tv <marco.sottana@gmail.com>
2022-04-01 16:58:20 +0200

    .

--- Resources/views/layouts/master.blade.php
+++ Resources/views/layouts/master.blade.php
@@ -1,41 +1,22 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Lang</title>
 
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/lang.css') }}"> --}}
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Module Lang</title>
 
-    </head>
-    <body>
-        @yield('content')
+    {{-- Laravel Mix - CSS File --}}
+    {{-- <link rel="stylesheet" href="{{ mix('css/lang.css') }}"> --}}
 
-        {{-- Laravel Mix - JS File --}}
-        {{-- <script src="{{ mix('js/lang.js') }}"></script> --}}
-    </body>
+</head>
+
+<body>
+    @yield('content')
+
+    {{-- Laravel Mix - JS File --}}
+    {{-- <script src="{{ mix('js/lang.js') }}"></script> --}}
+</body>
+
 </html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Module Lang</title>
-
-       {{-- Laravel Mix - CSS File --}}
-       {{-- <link rel="stylesheet" href="{{ mix('css/lang.css') }}"> --}}
-
-    </head>
-    <body>
-        @yield('content')
-
-        {{-- Laravel Mix - JS File --}}
-        {{-- <script src="{{ mix('js/lang.js') }}"></script> --}}
-    </body>
-</html>
->>>>>>> 7d158231dbb15c2a7f78061d1759eaca0a8a011e
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const dotenvExpand = require('dotenv-expand');
 dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
 
@@ -13,19 +12,3 @@ mix.js(__dirname + '/Resources/assets/js/app.js', 'js/lang.js')
 if (mix.inProduction()) {
     mix.version();
 }
-=======
-const dotenvExpand = require('dotenv-expand');
-dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
-
-const mix = require('laravel-mix');
-require('laravel-mix-merge-manifest');
-
-mix.setPublicPath('../../public').mergeManifest();
-
-mix.js(__dirname + '/Resources/assets/js/app.js', 'js/lang.js')
-    .sass(__dirname + '/Resources/assets/sass/app.scss', 'css/lang.css');
-
-if (mix.inProduction()) {
-    mix.version();
-}
->>>>>>> 7d158231dbb15c2a7f78061d1759eaca0a8a011e
