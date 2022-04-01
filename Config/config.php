ccd803d8e8558e79bd23916aeb62355e2d6a797c
marco76tv <marco.sottana@gmail.com>
2022-04-01 16:45:29 +0200

    .

--- Config/config.php
+++ Config/config.php
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 <?php
 
 return [
     'name' => 'Lang'
 ];
-=======
-<?php
-
-return [
-    'name' => 'Lang'
-];
->>>>>>> 7d158231dbb15c2a7f78061d1759eaca0a8a011e
--- Routes/api.php
+++ Routes/api.php
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
 <?php
+
+declare(strict_types=1);
 
 use Illuminate\Http\Request;
 
@@ -19,25 +20,3 @@ Route::middleware('auth:api')->get(
         return $request->user();
     }
 );
-=======
-<?php
-
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get(
-    '/lang', function (Request $request) {
-        return $request->user();
-    }
-);
->>>>>>> 7d158231dbb15c2a7f78061d1759eaca0a8a011e
