9dcdce798cb9654e2f45071def8f79e2743c3310
marco76tv <marco.sottana@gmail.com>
2022-04-01 16:45:29 +0200

    .

--- Models/BaseMorphPivot.php
+++ Models/BaseMorphPivot.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -60,66 +59,3 @@ abstract class BaseMorphPivot extends MorphPivot
         'note',
     ];
 }
-=======
-<?php
-
-declare(strict_types=1);
-
-namespace Modules\Lang\Models;
-
-use Illuminate\Database\Eloquent\Relations\MorphPivot;
-use Modules\Xot\Traits\Updater;
-
-/**
- * Class BaseMorphPivot.
- */
-abstract class BaseMorphPivot extends MorphPivot
-{
-    use Updater;
-
-    protected $connection = 'mysql'; // this will use the specified database connection
-
-    /**
-     * @var array
-     */
-    protected $appends = [];
-
-    /**
-     * @var string
-     */
-    protected $primaryKey = 'id';
-
-    /**
-     * @var bool
-     */
-    public $incrementing = true;
-
-    /**
-     * @var bool
-     */
-    public $timestamps = true;
-
-    //protected $attributes = ['related_type' => 'cuisine_cat'];
-
-    /**
-     * @var string[]
-     */
-    protected $dates = [
-        'created_at',
-        'updated_at',
-        'deleted_at',
-        // 'published_at',
-    ];
-
-    /**
-     * @var string[]
-     */
-    protected $fillable = [
-        'id',
-        'post_id', 'post_type',
-        'related_type',
-        'user_id',
-        'note',
-    ];
-}
->>>>>>> 7d158231dbb15c2a7f78061d1759eaca0a8a011e
--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
@@ -18,24 +17,3 @@ class RouteServiceProvider extends XotBaseRouteServiceProvider
 
     protected string $module_ns = __NAMESPACE__;
 }
-=======
-<?php
-
-declare(strict_types=1);
-
-namespace Modules\Lang\Providers;
-
-use Modules\Xot\Providers\XotBaseRouteServiceProvider;
-
-class RouteServiceProvider extends XotBaseRouteServiceProvider
-{
-    /**
-     * The module namespace to assume when generating URLs to actions.
-     */
-    protected string $moduleNamespace = 'Modules\Lang\Http\Controllers';
-
-    protected string $module_dir = __DIR__;
-
-    protected string $module_ns = __NAMESPACE__;
-}
->>>>>>> 7d158231dbb15c2a7f78061d1759eaca0a8a011e
