7e8fd139067d1c0cd9cdb91721e45994d979211a
marco76tv <marco.sottana@gmail.com>
2022-04-01 13:18:59 +0200

    .

---
+++ Models/BaseMorphPivot.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Lang\Models;
+
+use Illuminate\Database\Eloquent\Relations\MorphPivot;
+use Modules\Xot\Traits\Updater;
+
+/**
+ * Class BaseMorphPivot.
+ */
+abstract class BaseMorphPivot extends MorphPivot
+{
+    use Updater;
+
+    protected $connection = 'mysql'; // this will use the specified database connection
+
+    /**
+     * @var array
+     */
+    protected $appends = [];
+
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * @var bool
+     */
+    public $timestamps = true;
+
+    //protected $attributes = ['related_type' => 'cuisine_cat'];
+
+    /**
+     * @var string[]
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        // 'published_at',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'id',
+        'post_id', 'post_type',
+        'related_type',
+        'user_id',
+        'note',
+    ];
+}
