90c30205207d55d8ff2a5c8118169d33915a1681
Malebestia <malebestia@yahoo.it>
2022-05-09 19:19:37 +0200

    first

---
+++ Models/BaseModel.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Lang\Models;
+
+//use GeneaLabs\LaravelModelCaching\Traits\Cachable;
+//use Laravel\Scout\Searchable;
+//---------- traits
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Modules\Xot\Services\FactoryService;
+use Modules\Xot\Traits\Updater;
+
+/**
+ * Class BaseModel.
+ */
+abstract class BaseModel extends Model
+{
+    use Updater;
+    //use Searchable;
+    //use Cachable;
+    use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $connection = 'mysql'; // this will use the specified database conneciton
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['id'];
+    /**
+     * @var array
+     */
+    protected $casts = [
+        //'published_at' => 'datetime:Y-m-d', // da verificare
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $dates = ['published_at', 'created_at', 'updated_at'];
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    /**
+     * @var bool
+     */
+    public $incrementing = true;
+    /**
+     * @var array
+     */
+    protected $hidden = [
+        //'password'
+    ];
+    /**
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * Create a new factory instance for the model.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    protected static function newFactory()
+    {
+        return FactoryService::newFactory(get_called_class());
+    }
+}
