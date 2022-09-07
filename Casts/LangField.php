3a2f1583e872cd8b117a707d37a762b7aa66e2aa
marco76tv <marco.sottana@gmail.com>
2022-09-07 13:20:34 +0200

    rebase

--- Casts/LangField.php
+++ Casts/LangField.php
@@ -5,18 +5,30 @@ declare(strict_types=1);
 namespace Modules\Lang\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+<<<<<<< HEAD
 use Modules\Lang\Models\BaseModelLang;
+=======
+>>>>>>> 87192de (.)
 
 class LangField implements CastsAttributes {
     /**
      * Cast the given value.
      *
+<<<<<<< HEAD
      * @param BaseModelLang $model
      * @param string        $key
      * @param mixed         $value
      * @param array         $attributes
      *
      * @return mixed
+=======
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string                              $key
+     * @param mixed                               $value
+     * @param array                               $attributes
+     *
+     * @return \App\Models\Address
+>>>>>>> 87192de (.)
      */
     public function get($model, $key, $value, $attributes) {
         return $model->post->{$key};
@@ -25,10 +37,17 @@ class LangField implements CastsAttributes {
     /**
      * Prepare the given value for storage.
      *
+<<<<<<< HEAD
      * @param BaseModelLang $model
      * @param string        $key
      * @param mixed         $value
      * @param array         $attributes
+=======
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string                              $key
+     * @param \App\Models\Address                 $value
+     * @param array                               $attributes
+>>>>>>> 87192de (.)
      *
      * @return array
      */
