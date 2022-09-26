e91844a4980dcfb037c74dd65d045c2193cc8f69
marco76tv <marco.sottana@gmail.com>
2022-09-26 10:38:58 +0200

    .

--- Models/Panels/Policies/_ModulePanelPolicy.php
+++ Models/Panels/Policies/_ModulePanelPolicy.php
@@ -1,9 +1,18 @@
 <?php
+<<<<<<< HEAD
 
 declare(strict_types=1);
 
 namespace Modules\Lang\Models\Panels\Policies;
 
+=======
+namespace Modules\Lang\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Lang\Models\Panels\Policies\_ModulePanelPolicy as Panel;
+
+>>>>>>> c8a63ea (up)
 use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
 
 class _ModulePanelPolicy extends XotBasePanelPolicy {
