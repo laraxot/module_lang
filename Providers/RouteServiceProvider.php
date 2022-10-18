5932495d4063aa0bc3b9ee0bd64942cb74444121
marco76tv <marco.sottana@gmail.com>
2022-10-18 10:06:43 +0200

    .

--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -1,3 +1,30 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Lang\Providers;
+
+use Modules\Xot\Providers\XotBaseRouteServiceProvider;
+
+class RouteServiceProvider extends XotBaseRouteServiceProvider
+{
+    /**
+     * The module namespace to assume when generating URLs to actions.
+     */
+    protected string $moduleNamespace = 'Modules\Lang\Http\Controllers';
+
+    protected string $module_dir = __DIR__;
+
+    protected string $module_ns = __NAMESPACE__;
+}
+=======
+=======
+>>>>>>> 7c8dc60 (.)
+>>>>>>> 5801be3 (up)
 <?php
 
 declare(strict_types=1);
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,25 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+const dotenvExpand = require('dotenv-expand');
+dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
+
+const mix = require('laravel-mix');
+require('laravel-mix-merge-manifest');
+
+mix.setPublicPath('../../public').mergeManifest();
+
+mix.js(__dirname + '/Resources/assets/js/app.js', 'js/lang.js')
+    .sass(__dirname + '/Resources/assets/sass/app.scss', 'css/lang.css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
+=======
+=======
+>>>>>>> eec8c0a (.)
+>>>>>>> 5801be3 (up)
 const dotenvExpand = require('dotenv-expand');
 dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
 
