3bc32f6640674385ed5d8b5387d7ed72ff8443af
marco76tv <marco.sottana@gmail.com>
2022-10-18 09:00:29 +0200

    rebase

--- Models/Panels/Policies/PostPanelPolicy.php
+++ Models/Panels/Policies/PostPanelPolicy.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 <<<<<<< HEAD
 
@@ -17,3 +18,16 @@ use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
 
 class PostPanelPolicy extends XotBasePanelPolicy {
 }
+=======
+<?php
+namespace Modules\Lang\Models\Panels\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Modules\LU\Models\User as User;
+use Modules\Lang\Models\Panels\Policies\PostPanelPolicy as Panel;
+
+use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
+
+class PostPanelPolicy extends XotBasePanelPolicy {
+}
+>>>>>>> cfb7936 (.)
