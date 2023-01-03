47ee57f8ff51168514d340c68b8190fac507486c
marco76tv <marco.sottana@gmail.com>
2023-01-03 11:46:48 +0100

    up

---
+++ Http/Livewire/ManageLangModule.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Lang\Http\Livewire;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Lang;
+use Livewire\Component;
+use Modules\Xot\Services\ArrayService;
+use Modules\Xot\Services\FileService;
+use Nwidart\Modules\Facades\Module;
+
+/**
+ * Class ManageLangModule.
+ */
+class ManageLangModule extends Component {
+    public string $module_name;
+    public string $lang_name;
+    public string $path;
+
+    /**
+     * Listener di eventi di Livewire.
+     *
+     * @var array
+     */
+    protected $listeners = ['updateArray'];
+
+    public function mount(string $module_name): void {
+        $this->module_name = $module_name;
+        $lang = app()->getLocale();
+        $path = Module::getModulePath($this->module_name);
+        $path .= 'Resources\lang\\'.$lang.'';
+        $path = FileService::fixPath($path);
+        $this->path = $path;
+    }
+
+    /**
+     * Undocumented function.
+     */
+    public function render(): Renderable {
+        // $model->translations  ???
+
+        $files = File::files($this->path);
+        $files = collect($files)->filter(
+            function ($file) {
+                return 'php' === $file->getExtension();
+            }
+        );
+
+        /**
+         * @phpstan-var view-string
+         */
+        $view = 'xot::livewire.manage_lang_module';
+        $view_params = [
+            'view' => $view,
+            'files' => $files,
+            'prefix' => null,
+        ];
+
+        return view()->make($view, $view_params);
+    }
+
+    /**
+     * Undocumented function.
+     */
+    public function edit(string $lang_name): void {
+        $this->lang_name = $lang_name;
+        $mod_trad = $this->module_name.'::'.$this->lang_name;
+        $form_data = Lang::get($mod_trad, []); // progressioni::prova
+
+        // $form_data = File::getRequire($this->path.'/'.$lang_name.'.php');
+
+        $this->emit('editModalArray', $form_data);
+    }
+
+    /**
+     * Undocumented function.
+     */
+    public function updateArray(array $form_data): void {
+        $filename = $this->path.'/'.$this->lang_name.'.php';
+        ArrayService::save(['filename' => $filename, 'data' => $form_data]);
+    }
+}
---
+++ Models/Panels/Actions/ManageLangModuleAction.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Lang\Models\Panels\Actions;
+/*
+use Nwidart\Modules\Facades\Module;
+use Illuminate\Support\Facades\File;
+use Modules\Xot\Services\FileService;
+*/
+use Modules\Cms\Models\Panels\Actions\XotBasePanelAction;
+
+
+
+/**
+ * Class ManageLangModuleAction.
+ */
+class ManageLangModuleAction extends XotBasePanelAction {
+    public bool $onContainer = true; // onlyContainer
+
+    public bool $onItem = true; // onlyContainer
+
+    public string $icon = '<i class="fas fa-language"></i>';
+
+    public string $module_name;
+
+    public function __construct(string $module_name) {
+        $this->module_name = $module_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function handle() {
+        /*$module = Module::find($this->module_name);
+        dddx(
+            [
+                'module' => $module,
+                'Module::getModulePath(name);' => Module::getModulePath($this->module_name),
+                'Module::assetPath(name);' => Module::assetPath($this->module_name),
+
+                //'getUsedStoragePath' => $module->getUsedStoragePath(),//Laravel\Module::getUsedStoragePath does not exist.
+                //'getAssetsPath' => $module->getAssetsPath(),//dart\Modules\Laravel\Module::getAssetsPath does not exist.
+            ]);
+        */
+        /*
+        $lang = app()->getLocale();
+        $path = Module::getModulePath($this->module_name);
+        $path .= 'Resources\lang\\'.$lang.'';
+        $path = FileService::fixPath($path);
+
+        $files = File::files($path);
+        */
+        $view_params = [
+            // 'files' => $files,
+            'module_name' => $this->module_name,
+        ];
+
+        return $this->panel->view()->with($view_params);
+    }
+
+    // end handle
+}
