9ca2e9c4c23eaafc4a869704694c2abd79b6e93c
marco76tv <marco76tv@users.noreply.github.com>
2023-01-25 14:01:43 +0000

    Fix styling

--- Models/Panels/Policies/PostPanelPolicy.php
+++ Models/Panels/Policies/PostPanelPolicy.php
@@ -6,6 +6,5 @@ namespace Modules\Lang\Models\Panels\Policies;
 
 use Modules\Cms\Models\Panels\Policies\XotBasePanelPolicy;
 
-class PostPanelPolicy extends XotBasePanelPolicy
-{
+class PostPanelPolicy extends XotBasePanelPolicy {
 }
