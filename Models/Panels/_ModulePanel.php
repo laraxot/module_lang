4731f0733a58d3db661a19fc51f98430573d22df
marco76tv <marco.sottana@gmail.com>
2023-02-01 18:29:38 +0100

    up

--- Models/Panels/_ModulePanel.php
+++ Models/Panels/_ModulePanel.php
@@ -9,8 +9,10 @@ use Modules\Cms\Models\Panels\XotBasePanel;
 /**
  * Class _ModulePanel.
  */
-class _ModulePanel extends XotBasePanel {
-    public function actions(): array {
+class _ModulePanel extends XotBasePanel
+{
+    public function actions(): array
+    {
         return [
             // new Actions\TestAction(),
             // new Actions\TestAction(),
