03ab99ccf0cfa8419f491308595ac820e62ba337
marco76tv <marco.sottana@gmail.com>
2023-03-08 15:45:49 +0100

    up

--- Models/Panels/PostPanel.php
+++ Models/Panels/PostPanel.php
@@ -46,7 +46,7 @@ class PostPanel extends XotBasePanel {
      *
      * @return RowsContract
      */
-    public static function indexQuery(array $data, $query) {
+    public function indexQuery(array $data, $query) {
         // return $query->where('user_id', $request->user()->id);
         return $query;
     }
