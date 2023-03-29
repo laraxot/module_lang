f69d8a976cada9546483328d9fd9d820bbf665a0
marco xot <marco.sottana@gmail.com>
2023-03-29 11:05:10 +0200

    up

--- Models/Translation.php
+++ Models/Translation.php
@@ -14,18 +14,17 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Translation model.
  *
- * @property int                             $id
- * @property string|null                     $lang
- * @property string|null                     $key
- * @property string|null                     $value
- * @property string|null                     $created_by
- * @property string|null                     $updated_by
+ * @property int $id
+ * @property string|null $lang
+ * @property string|null $key
+ * @property string|null $value
+ * @property string|null $created_by
+ * @property string|null $updated_by
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string                          $namespace
- * @property string                          $group
- * @property string|null                     $item
- *
+ * @property string $namespace
+ * @property string $group
+ * @property string|null $item
  * @method static \Illuminate\Database\Eloquent\Builder|Translation newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Translation newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Translation ofTranslatedGroup($group)
@@ -43,7 +42,6 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Translation whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Translation whereUpdatedBy($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Translation whereValue($value)
- *
  * @mixin \Eloquent
  */
 class Translation extends Model {
