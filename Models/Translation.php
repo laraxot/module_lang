b50a288b6f3406e410ceb31ac1981f89556690f6
marco76tv <marco.sottana@gmail.com>
2023-03-29 10:18:18 +0200

    up

--- Models/Translation.php
+++ Models/Translation.php
@@ -14,14 +14,35 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * Translation model.
  *
- * @property int            $id
- * @property int            $status
- * @property string         $locale
- * @property string         $group
- * @property string         $key
- * @property string         $value
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
+ * @property int $id
+ * @property string|null $lang
+ * @property string|null $key
+ * @property string|null $value
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $namespace
+ * @property string $group
+ * @property string|null $item
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation ofTranslatedGroup($group)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation orderByGroupKeys($ordered)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation selectDistinctGroup()
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereGroup($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereItem($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereKey($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereLang($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereNamespace($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereUpdatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Translation whereValue($value)
+ * @mixin \Eloquent
  */
 class Translation extends Model {
     protected $fillable = ['id', 'lang', 'key', 'value', 'created_by', 'updated_by', 'created_at', 'updated_at', 'namespace', 'group', 'item'];
