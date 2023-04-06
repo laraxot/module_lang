465fd9b14f2244d243b244f48db2286261aab1ed
marco xot <marco.sottana@gmail.com>
2023-04-06 18:17:31 +0200

    up

---
+++ View/Components/Flag.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Lang\View\Components;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\View\Component;
+use Modules\Cms\Actions\GetViewAction;
+
+/**
+ * Undocumented class.
+ */
+class Flag extends Component {
+    public string $name;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function render(): Renderable {
+        $view = app(GetViewAction::class)->execute();
+        $view_params = [
+            'view' => $view,
+        ];
+
+        return view($view, $view_params);
+    }
+}
