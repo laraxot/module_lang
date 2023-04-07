5c39a3209fd2aede022460197c0618074b72513d
marco76tv <marco.sottana@gmail.com>
2023-04-07 18:05:05 +0200

    up

--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -6,8 +6,7 @@ namespace Modules\Lang\Providers;
 
 use Modules\Xot\Providers\XotBaseRouteServiceProvider;
 
-class RouteServiceProvider extends XotBaseRouteServiceProvider
-{
+class RouteServiceProvider extends XotBaseRouteServiceProvider {
     /**
      * The module namespace to assume when generating URLs to actions.
      */
@@ -16,4 +15,39 @@ class RouteServiceProvider extends XotBaseRouteServiceProvider
     protected string $module_dir = __DIR__;
 
     protected string $module_ns = __NAMESPACE__;
+
+    public function bootCallback(): void {
+        $this->registerLang();
+    }
+
+    public function registerCallback(): void {
+        // $this->registerLang();
+    }
+
+    public function registerLang(): void {
+        $locales = config('laravellocalization.supportedLocales');
+        if (! \is_array($locales)) {
+            // throw new \Exception('[.__LINE__.]['.class_basename(__CLASS__).']');
+            $locales = ['it' => 'it', 'en' => 'en'];
+        }
+        $langs = array_keys($locales);
+
+        if (! \is_array($langs)) {
+            throw new \Exception('[.__LINE__.]['.class_basename(__CLASS__).']');
+        }
+        $route_params = getRouteParameters();
+        $n = 1;
+        if (inAdmin()) {
+            $n = 3;
+        }
+
+        if (\in_array(request()->segment($n), $langs, true)) {
+            $lang = request()->segment($n);
+            if (null !== $lang) {
+                // App::setLocale($lang);
+
+                app()->setLocale($lang);
+            }
+        }
+    }
 }
---
+++ View/Composers/ThemeComposer.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Lang\View\Composers;
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * --.
+ */
+class ThemeComposer {
+    public function languages() {
+        $lang = app()->getLocale();
+        $langs = config('laravellocalization.supportedLocales');
+        $langs = collect($langs)->map(
+            function ($item, $k) {
+                $reg = collect(explode('_', $item['regional']))->first();
+                if ('en' == $reg) {
+                    $reg = 'gb';
+                }
+                $url = '#'; // devo fare ancora front
+                if (inAdmin()) {
+                    $route_name = Route::currentRouteName();
+                    $route_parameters = getRouteParameters();
+                    $data = request()->all();
+                    $route_parameters['lang'] = $k;
+                    $url = route($route_name, $route_parameters);
+                    $url = \Illuminate\Http\Request::create($url)->fullUrlWithQuery($data);
+                }
+
+                return [
+                    'id' => $k,
+                    'name' => $item['name'],
+                    'flag' => '<div class="iti__flag-box"><div class="iti__flag iti__'.$reg.'"></div></div>',
+                    'url' => $url,
+                ];
+            }
+        );
+
+        return $langs;
+    }
+
+    public function otherLanguages() {
+        $curr = app()->getLocale();
+        $langs = $this->languages()
+            ->filter(function ($item) use ($curr) {
+                return $item['id'] != $curr;
+            });
+
+        return $langs;
+    }
+
+    public function currentLang(string $field) {
+        $curr = app()->getLocale();
+        $lang = $this->languages()->get($curr);
+
+        return $lang[$field];
+    }
+}
