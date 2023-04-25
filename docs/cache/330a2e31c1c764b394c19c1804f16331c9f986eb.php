c30b596183be5a1e7cb3e38770c6ce847ece7a86
marco76tv <marco76tv@users.noreply.github.com>
2023-04-25 14:47:56 +0000

    Fix styling

--- docs/cache/330a2e31c1c764b394c19c1804f16331c9f986eb.php
+++ docs/cache/330a2e31c1c764b394c19c1804f16331c9f986eb.php
@@ -3,11 +3,16 @@
 <?php $__env->startSection('body'); ?>
     <h1>Blog Posts</h1>
     
-    <?php $__currentLoopData = $posts->where('lang',$page->lang); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <?php $__currentLoopData = $posts->where('lang', $page->lang);
+$__env->addLoop($__currentLoopData);
+foreach ($__currentLoopData as $post) {
+    $__env->incrementLoopIndices();
+    $loop = $__env->getLastLoop(); ?>
         <h2><a href="<?php echo e($post->getPath()); ?>"><?php echo e($post->title); ?></a></h2>
         <h3>By <?php echo e($post->author); ?></h3>
         <?php echo e($post->getContent()); ?>
 
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    <?php } $__env->popLoop();
+$loop = $__env->getLastLoop(); ?>
 <?php $__env->stopSection(); ?>
 <?php echo $__env->make('_layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /var/www/html/_bases/base_pfed/laravel/Modules/Lang/docs/source/posts.blade.php ENDPATH**/ ?>
