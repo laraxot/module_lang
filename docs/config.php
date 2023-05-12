ea602241fdc62a8d86a4942cbc8adabb90d9ce99
marco76tv <marco76tv@users.noreply.github.com>
2023-05-12 15:29:36 +0000

    Fix styling

--- docs/config.php
+++ docs/config.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Str;
 
 $moduleName = 'Lang';
@@ -9,20 +11,20 @@ return [
     'production' => false,
     'siteName' => 'Modulo '.$moduleName,
     'siteDescription' => 'Modulo '.$moduleName,
-    //'lang' => 'it',
+    // 'lang' => 'it',
 
     'collections' => [
         'posts' => [
             'path' => function ($page) {
-                //return $page->lang.'/posts/'.Str::slug($page->getFilename());
-                //return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
+                // return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                // return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
 
                 return 'posts/'.Str::slug($page->getFilename());
             },
         ],
         'docs' => [
             'path' => function ($page) {
-                //return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                // return $page->lang.'/docs/'.Str::slug($page->getFilename());
                 return 'docs/'.Str::slug($page->getFilename());
             },
         ],
@@ -45,7 +47,7 @@ return [
                 return trimPath($page->getPath()) == trimPath($child);
             });
         }
-    },/*
+    }, /*
     'url' => function ($page, $path) {
         return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
     },
@@ -54,7 +56,7 @@ return [
         if (Str::startsWith($path, 'http')) {
             return $path;
         }
-         //return url('/'.$page->lang.'/'.trimPath($path));
+        // return url('/'.$page->lang.'/'.trimPath($path));
         return url('/'.trimPath($path));
     },
 ];
