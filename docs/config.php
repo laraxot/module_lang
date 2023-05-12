51e5a7b5b513a613551c5ff82d2eb555ab106293
marco76tv <marco.sottana@gmail.com>
2023-05-12 17:27:55 +0200

    up

--- docs/config.php
+++ docs/config.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 use Illuminate\Support\Str;
 
 $moduleName = 'Lang';
@@ -11,17 +9,21 @@ return [
     'production' => false,
     'siteName' => 'Modulo '.$moduleName,
     'siteDescription' => 'Modulo '.$moduleName,
-    'lang' => 'it',
+    //'lang' => 'it',
 
     'collections' => [
         'posts' => [
             'path' => function ($page) {
-                return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                //return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                //return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
+
+                return 'posts/'.Str::slug($page->getFilename());
             },
         ],
         'docs' => [
             'path' => function ($page) {
-                return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                //return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                return 'docs/'.Str::slug($page->getFilename());
             },
         ],
     ],
@@ -43,12 +45,16 @@ return [
                 return trimPath($page->getPath()) == trimPath($child);
             });
         }
+    },/*
+    'url' => function ($page, $path) {
+        return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
     },
+    */
     'url' => function ($page, $path) {
         if (Str::startsWith($path, 'http')) {
             return $path;
         }
-        // return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
-        return url('/'.$page->lang.'/'.trimPath($path));
+         //return url('/'.$page->lang.'/'.trimPath($path));
+        return url('/'.trimPath($path));
     },
 ];
