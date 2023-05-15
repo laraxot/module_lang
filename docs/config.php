1f92d907f9e14c2427c8b6602795610adea4564a
marco76tv <marco76tv@users.noreply.github.com>
2023-05-15 10:13:52 +0000

    Fix styling

--- docs/config.php
+++ docs/config.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Str;
 
 $moduleName = 'Lang';
@@ -14,15 +16,15 @@ return [
     'collections' => [
         'posts' => [
             'path' => function ($page) {
-                //return $page->lang.'/posts/'.Str::slug($page->getFilename());
-                //return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
+                // return $page->lang.'/posts/'.Str::slug($page->getFilename());
+                // return 'posts/' . ($page->featured ? 'featured/' : '') . Str::slug($page->getFilename());
 
                 return 'posts/'.Str::slug($page->getFilename());
             },
         ],
         'docs' => [
             'path' => function ($page) {
-                //return $page->lang.'/docs/'.Str::slug($page->getFilename());
+                // return $page->lang.'/docs/'.Str::slug($page->getFilename());
                 return 'docs/'.Str::slug($page->getFilename());
             },
         ],
@@ -48,7 +50,7 @@ return [
                 return trimPath($page->getPath()) == trimPath($child);
             });
         }
-    },/*
+    }, /*
     'url' => function ($page, $path) {
         return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
     },
@@ -57,7 +59,7 @@ return [
         if (Str::startsWith($path, 'http')) {
             return $path;
         }
-         //return url('/'.$page->lang.'/'.trimPath($path));
+        // return url('/'.$page->lang.'/'.trimPath($path));
         return url('/'.trimPath($path));
     },
 
